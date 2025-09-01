9ad453c8074457e2a95f3fc10a51c0cd0bd3fa8a
g.libin <g.libin@lipt-soft.ru>
2025-09-01 08:33:26 +0300

    add module kafka

---
+++ yc/kafka/examples/ha cluster/cluster.tf
@@ -0,0 +1,36 @@
+module "kafka" {
+  source = "../../"
+
+  security_groups_ids_list = [yandex_vpc_security_group.db_sg.id, ]
+
+  network_id = yandex_vpc_network.vpc.id
+  subnet_ids = [yandex_vpc_subnet.sub_a.id, yandex_vpc_subnet.sub_b.id, yandex_vpc_subnet.sub_d.id, ]
+  zones      = ["ru-central1-a", "ru-central1-b", "ru-central1-d"]
+  kafka_config = {
+    compression_type = "COMPRESSION_TYPE_ZSTD"
+  }
+  connectors = []
+  topics = [
+    {
+      name                = "events"
+      partitions          = 1
+      replication_factor  = 3
+      min_insync_replicas = 2
+    }
+  ]
+
+  users = [
+    {
+      name = "test1-owner"
+      permissions = [
+        { "topic_name" : "events"
+        "role" : "ACCESS_ROLE_CONSUMER" }
+      ]
+    },
+  ]
+
+  maintenance_window = {
+    type : "ANYTIME"
+  }
+
+}
