3241d69c13c4d9873a1aecab43446669bbf1d010
g.libin <g.libin@lipt-soft.ru>
2026-02-11 09:55:10 +0300

    fix secrets providers fixed

--- k8s/secrets/versions.tf
+++ k8s/secrets/versions.tf
@@ -3,15 +3,15 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.8.0"
+      version = ">= 2.8.0, < 3.0.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.4.1"
+      version = ">= 3.0.0, < 4.0.0"
     }
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = ">= 0.65"
+      version = ">= 0.65, < 1.0"
     }
   }
 }
