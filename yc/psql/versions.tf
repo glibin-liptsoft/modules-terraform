7339bdb84ed1cfef1500c874af6d25f140c9b52e
g.libin <g.libin@lipt-soft.ru>
2026-03-03 11:05:35 +0300

    lock ver prov yandex for psql

--- yc/psql/versions.tf
+++ yc/psql/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = ">= 0.89.0"
+      version = ">= 0.127.0, < 0.190.0"
     }
 
     random = {
